1b2377c94e0390be342c2c7f72412381fbe74b15
Dani Kannanaikal Martin <116060128+DIDWDE@users.noreply.github.com>
2023-11-19 07:07:44 +0800

    changes made

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from py3dbp import Packer, Bin, Item
+import pandas as pd
+import numpy as np
 
 # Define the cargo plane dimensions
 cargo_plane_length = 80
@@ -10,6 +12,50 @@ packer = Packer()
 
 # Create a Bin representing the cargo plane
 bin = Bin("Cargo Plane", cargo_plane_length, cargo_plane_width, cargo_plane_height, max_weight= 1000)
+
+flag=False
+i=0
+j=0
+k=0
+while flag==False:
+    print("Please input type of good (dg,gen) or q for finished")
+    Typeofgood=input()
+    if Typeofgood=='q':
+        flag=True
+    else:
+        print(" Please enter length of good ")
+        length=input()
+        print(" Please enter width of good ")
+        width=input()
+        print(" Please enter height of good ")
+        height=input()
+        print(" Please enter weight of good ")
+        weight=input()
+
+        if Typeofgood == 'dg':
+            print (" Good assigned to Dangerous Goods Cargo ")
+            if i==0:
+                dg=np.array([length,width,height,weight])
+            else:
+                row_to_be_added = np.array([length,width,height,weight])
+                dg = np.vstack ((dg, row_to_be_added) )
+            i=+1
+        elif Typeofgood== 'gen':
+            print(" Good assigned to General Goods Cargo ")
+            if j ==0:
+                gen=np.array([length,width,height,weight])
+            else:
+                row_to_be_added = np.array([length,width,height,weight])
+                gen = np.vstack ((gen, row_to_be_added) )
+            j=+1
+        else:
+            print(" Goods have to been transferred to Miscallaneous Goods Cargo ")
+            if k ==0:
+                ms=np.array([length,width,height,weight])
+            else:
+                row_to_be_added = np.array([length,width,height,weight])
+                ms = np.vstack ((ms, row_to_be_added) )
+            k=+1
 
 # Create the dataset of cargo boxes
 cargo_boxes = [
